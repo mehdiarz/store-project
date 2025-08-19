13ff9c652ca0109ee0d8cf06259171ad960a71fd
mahdiarz <mahdi_arezoomand@yahoo.com>
2025-08-20 01:34:27 +0330

    chechout page

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,8 +1,35 @@
+import { useCart } from "../context/CartContext.jsx";
+import BasketCard from "../components/BasketCard.jsx";
+import BasketSideBar from "../components/BasketSideBar.jsx";
+import styles from "./ChekoutPage.module.css";
 
 const CheckoutPage = () => {
+  const [state, dispatch] = useCart();
+  const clickHandler = (type, data) => {
+    dispatch({ type, payload: data });
+  };
+
+  if (!state.itemsCounter) {
     return (
-        <div>CheckoutPage</div>
+      <div className={styles.container}>
+        <p>Empty</p>
+      </div>
     );
+  }
+  return (
+    <div className={styles.container}>
+        <BasketSideBar state={state} clickHandler={clickHandler} />
+      <div className={styles.products}>
+        {state.selectedItems.map((product) => (
+          <BasketCard
+            key={product.id}
+            data={product}
+            clickHandler={clickHandler}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default CheckoutPage;
